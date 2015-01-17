5a12ffe3c0b354723accf7c5ebcdc553588ef951
ken <kkituyi@yahoo.com>
2015-01-17 18:54:34 +0300

    Another better version

--- nwmxml.php
+++ nwmxml.php
@@ -1,11 +1,12 @@
+
 <?php
 header("Content-type: text/xml");
-echo "<?xml version='1.0' encoding='UTF-8'?>";
-echo "<markers>";
-echo "<marker host=\"192.168.1.1\" lastupdate=\"2012-03-12 18:02:54\" lat=\"44.805538\" lng=\"150.967235\" active=\"1\"/>";
-echo "<marker host=\"192.168.1.80\" lastupdate=\"2012-03-09 15:02:03\" lat=\"37.805450\" lng=\"140.967224\" active=\"0\"/>";
-echo "<marker host=\"192.168.1.2\" lastupdate=\"2012-03-12 18:03:30\" lat=\"37.805685\" lng=\"140.967224\" active=\"1\"/>";
-echo "<marker host=\"192.168.1.75\" lastupdate=\"2012-03-12 18:05:14\" lat=\"37.805685\" lng=\"140.967150\" active=\"0\"/>";
-echo "<marker host=\"192.168.1.67\" lastupdate=\"2012-03-12 18:07:04\" lat=\"37.805685\" lng=\"140.966995\" active=\"1\"/>";
-echo "</markers>";
+echo "<users>";
+echo "<user id=\"1\" name=\"John Tucker\" age=\"19\" lat=\"47.608940\" lng=\"-122.340141\" type=\"0\"/>";
+echo "<user id=\"2\" name=\"Dean Jester\" age=\"21\" lat=\"51.219040\" lng=\"4.326590\" type=\"0\"/>";
+echo "<user id=\"3\" name=\"Joris Nisteven\" age=\"26\" lat=\"51.203671\" lng=\"4.341480\" type=\"0\"/>";
+echo "<user id=\"4\" name=\"Joske Vermeulen\" age=\"20\" lat=\"51.204155\" lng=\"4.327018\" type=\"0\"/>";
+echo "<user id=\"5\" name=\"Timmy den Beir\" age=\"21\" lat=\"51.209263\" lng=\"4.339720\" type=\"0\"/>";
+echo "<user id=\"6\" name=\"Ben von Duppen\" age=\"23\" lat=\"51.168308\" lng=\"4.394287\" type=\"0\"/>";
+echo "</users>";
 ?>
