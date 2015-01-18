b09bf6bb7a1ca22717998446b49810365f2b238c
ken <kkituyi@yahoo.com>
2015-01-18 10:58:24 +0300

    Added the original in comment

--- nwmxml.php
+++ nwmxml.php
@@ -10,3 +10,9 @@ echo "<user id=\"5\" name=\"Timmy den Beir\" age=\"21\" lat=\"51.209263\" lng=\"
 echo "<user id=\"6\" name=\"Ben von Duppen\" age=\"23\" lat=\"51.168308\" lng=\"4.394287\" type=\"0\"/>";
 echo "</users>";
 ?>
+////////////////////////////Original//////
+//<?php
+//echo '<markers>
+//<marker host="192.168.1.1" lastupdate="2012-03-12 18:02:54" lat="37.805538" lng="140.967235" active="1"/><marker host="192.168.1.80" lastupdate="2012-03-09 15:02:03" lat="37.805450" lng="140.967224" active="0"/><marker host="192.168.1.2" lastupdate="2012-03-12 18:03:30" lat="37.805685" lng="140.967224" active="1"/><marker host="192.168.1.75" lastupdate="2012-03-12 18:05:14" lat="37.805685" lng="140.967150" active="0"/><marker host="192.168.1.67" lastupdate="2012-03-12 18:07:04" lat="37.805685" lng="140.966995" active="1"/></markers>';
+//?>
+//////////////////////////////////////////////////////
