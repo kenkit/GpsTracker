ca214aa7757fbbebaafd4a27e7dd3ad4edd51c8e
kenkit <kkituyi@yahoo.com>
2015-01-20 17:38:00 +0300

    Sql+Map is now live, only prob is firefox cache

--- getGpsPoints.php
+++ getGpsPoints.php
@@ -1,34 +1,61 @@
 <?php
-    header ("content-type: text/xml");
- 
-    $link = mysql_connect ("localhost", "root", "toor");
-    if (!$link) {
-        die('Could not connect: ' . mysql_error());
-    }
- 
-    mysql_select_db ("location");
- 
-    //If no search string is passed, then we can't search
-    if(empty($_GET["id"])) {
-        echo " ";
-    } else {
-        //Remove whitespace from beginning x%x% end of passed search.
-        $search = trim($_GET["id"]);
- 
-        //Query the DB and store the result in a variable
-        $query = mysql_query("SELECT * FROM gps WHERE id=".$search);
- 
-        //If no rows are found...
-        if(mysql_num_rows($query) == 0) {
-            echo " ";
-        } else {
-            //Stick the returned rows into a handy array for easy use
-            $row = mysql_fetch_array($query) or die(mysql_error()); 
- 
-            //Write out XML using values returned by the query
-            echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><markers><marker lat=\"".$row["lat"]."\" lng=\"".$row["lng"]."\" /></markers>";
-        }
-    }
- 
-    mysql_close($link);
+
+
+
+$username="root";
+$password="toor";
+$database="location";
+
+
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+// Opens a connection to a MySQL server
+$connection=mysql_connect ('localhost', $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+// Set the active MySQL database
+$db_selected = mysql_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM gps WHERE 1";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo '<markers>';
+
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // ADD TO XML DOCUMENT NODE
+  echo '<marker ';
+  echo 'host="' . parseToXML($row['host']) . '" ';
+  echo 'lastupdate="' . parseToXML($row['lastupdate']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'active="' . $row['active'] . '" ';
+  echo '/>';
+}
+
+// End XML file
+echo '</markers>';
+
 ?>
+
